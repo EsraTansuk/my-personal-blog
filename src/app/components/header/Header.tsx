e13a7a58b3b5d2a848a1f6f58b6f7b108389dce2
Esra Tansuk <esra.balkis@outlook.com>
2023-06-05 15:34:08 +0300

    text pading changed

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -30,7 +30,7 @@ export const Header = () => {
                   boxShadow: "box-shadow: 0 0 7px rgba(0,0,0,.9)",
                 }}
               ></div>
-              <div className="max-w-full lg:w-4/6 items-center text-center lg:text-left px:2 md:px-20 lg:px-28 mb-10">
+              <div className="max-w-full lg:w-4/6 items-center text-center lg:text-left px:2 md:px-20 lg:px-52 mb-10">
                 <h1 className=" text-5xl lg:text-5xl xl:text-6xl relative mt-6 uppercase text-mainColor font-bold ps-8">
                   I'm Esra Tansuk. <br/> <span className=" text-white my-4">Frontend Developer</span>
                 </h1>
