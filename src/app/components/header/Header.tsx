ec42f069646705cd3bf0adbb6389ed69a89dfeca
Esra Tansuk <esra.balkis@outlook.com>
2023-06-09 12:07:15 +0300

    portfolio tab added

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -12,6 +12,7 @@ import {
 import { Tabs } from "../tabItem/Tabs";
 import { Tab } from "../tabItem/Tab";
 import { Progressbar } from "./components/Progressbar";
+import Image from "next/image";
 
 export const Header: React.FC = () => {
   const htmlPercentage: number = 90;
@@ -550,10 +551,156 @@ export const Header: React.FC = () => {
                   WORKS
                 </span>
               </div>
-              <div className="w-full">
-                <div className="w-full mx-auto md:max-w-7xl flex flex-col xl:flex-row gap-12">
-                  <div>
-                    <img src="/img/project-1.webp" />
+              <div className="w-full mb-8">
+                <div className="w-full px-4 grid grid-cols-1  md:grid-cols-2 xl:grid-cols-3 gap-7 xl:mx-auto  ">
+                  <div className="w-full h-full">
+                    <div className="w-full h-64 relative cursor-pointer flex">
+                      <Image
+                        src="/img/project-1.webp"
+                        alt="project1"
+                        width={895}
+                        height={552}
+                        className="absolute top-0 left-0 w-full h-full rounded-lg "
+                      />
+
+                      <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full transition-all duration-700 flex justify-center items-center text-lg rounded-lg bg-mainColor opacity-0 hover:opacity-100">
+                        <span className="transition-all duration-500 ease-in-out transform relative ">
+                          MOCKUP PROJECT
+                        </span>
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="w-full h-full">
+                    <div className="w-full h-64 relative cursor-pointer flex">
+                      <Image
+                        src="/img/project-2.webp"
+                        alt="project1"
+                        width={895}
+                        height={552}
+                        className="absolute top-0 left-0 w-full h-full rounded-lg "
+                      />
+
+                      <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full transition-all duration-700 flex justify-center items-center text-lg rounded-lg bg-mainColor opacity-0 hover:opacity-100">
+                        <span className="transition-all duration-500 ease-in-out transform relative ">
+                          MOCKUP PROJECT
+                        </span>
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="w-full h-full">
+                    <div className="w-full h-64 relative cursor-pointer flex">
+                      <Image
+                        src="/img/project-3.webp"
+                        alt="project1"
+                        width={895}
+                        height={552}
+                        className="absolute top-0 left-0 w-full h-full rounded-lg "
+                      />
+
+                      <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full transition-all duration-700 flex justify-center items-center text-lg rounded-lg bg-mainColor opacity-0 hover:opacity-100">
+                        <span className="transition-all duration-500 ease-in-out transform relative ">
+                          MOCKUP PROJECT
+                        </span>
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="w-full h-full">
+                    <div className="w-full h-64 relative cursor-pointer flex">
+                      <Image
+                        src="/img/project-4.webp"
+                        alt="project1"
+                        width={895}
+                        height={552}
+                        className="absolute top-0 left-0 w-full h-full rounded-lg "
+                      />
+
+                      <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full transition-all duration-700 flex justify-center items-center text-lg rounded-lg bg-mainColor opacity-0 hover:opacity-100">
+                        <span className="transition-all duration-500 ease-in-out transform relative ">
+                          MOCKUP PROJECT
+                        </span>
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="w-full h-full">
+                    <div className="w-full h-64 relative cursor-pointer flex">
+                      <Image
+                        src="/img/project-5.webp"
+                        alt="project1"
+                        width={895}
+                        height={552}
+                        className="absolute top-0 left-0 w-full h-full rounded-lg "
+                      />
+
+                      <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full transition-all duration-700 flex justify-center items-center text-lg rounded-lg bg-mainColor opacity-0 hover:opacity-100">
+                        <span className="transition-all duration-500 ease-in-out transform relative ">
+                          MOCKUP PROJECT
+                        </span>
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="w-full h-full">
+                    <div className="w-full h-64 relative cursor-pointer flex">
+                      <Image
+                        src="/img/project-6.webp"
+                        alt="project1"
+                        width={895}
+                        height={552}
+                        className="absolute top-0 left-0 w-full h-full rounded-lg "
+                      />
+                      <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full transition-all duration-700 flex justify-center items-center text-lg rounded-lg bg-mainColor opacity-0 hover:opacity-100">
+                        <span className="transition-all duration-500 ease-in-out transform relative ">
+                          MOCKUP PROJECT
+                        </span>
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="w-full h-full">
+                    <div className="w-full h-64 relative cursor-pointer flex">
+                      <Image
+                        src="/img/project-7.webp"
+                        alt="project1"
+                        width={895}
+                        height={552}
+                        className="absolute top-0 left-0 w-full h-full rounded-lg "
+                      />
+                      <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full transition-all duration-700 flex justify-center items-center text-lg rounded-lg bg-mainColor opacity-0 hover:opacity-100">
+                        <span className="transition-all duration-500 ease-in-out transform relative ">
+                          MOCKUP PROJECT
+                        </span>
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="w-full h-full">
+                    <div className="w-full h-64 relative cursor-pointer flex">
+                      <Image
+                        src="/img/project-8.webp"
+                        alt="project1"
+                        width={895}
+                        height={552}
+                        className="absolute top-0 left-0 w-full h-full rounded-lg "
+                      />
+                      <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full transition-all duration-700 flex justify-center items-center text-lg rounded-lg bg-mainColor opacity-0 hover:opacity-100">
+                        <span className="transition-all duration-500 ease-in-out transform relative ">
+                          MOCKUP PROJECT
+                        </span>
+                      </h3>
+                    </div>
+                  </div>
+                  <div className="w-full h-full">
+                    <div className="w-full h-64 relative cursor-pointer flex">
+                      <Image
+                        src="/img/project-9.webp"
+                        alt="project1"
+                        width={895}
+                        height={552}
+                        className="absolute top-0 left-0 w-full h-full rounded-lg "
+                      />
+                      <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full transition-all duration-700 flex justify-center items-center text-lg rounded-lg bg-mainColor opacity-0 hover:opacity-100">
+                        <span className="transition-all duration-500 ease-in-out transform relative ">
+                          MOCKUP PROJECT
+                        </span>
+                      </h3>
+                    </div>
                   </div>
                 </div>
               </div>
