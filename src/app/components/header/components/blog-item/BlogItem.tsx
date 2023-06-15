389d75a23053c22bc88f4a6772c0150e907bfa96
Esra Tansuk <esra.balkis@outlook.com>
2023-06-15 16:46:05 +0300

    blogItem class changed

--- src/app/components/header/components/blog-item/BlogItem.tsx
+++ src/app/components/header/components/blog-item/BlogItem.tsx
@@ -10,7 +10,7 @@ interface BlogItemProps {
 
 export const BlogItem: React.FC<BlogItemProps>  = ({title,text,src,alt}) => {
   return (
-    <div className="block cursor-pointer overflow-hidden rounded-md">
+    <div className="block cursor-pointer overflow-hidden rounded-md hover:text-mainColor">
       <div className=" border-b-[5px] border-mainColor rounded-t-md">
         <div className="relative overflow-hidden block h-64">
           <Image
@@ -18,13 +18,13 @@ export const BlogItem: React.FC<BlogItemProps>  = ({title,text,src,alt}) => {
             alt={alt}
             width={895}
             height={552}
-            className="absolute top-0 left-0 w-full h-full rounded-lg"
+            className="absolute top-0 left-0 w-full h-full rounded-lg hover:scale-110"
           />
         </div>
       </div>
-      <div className=" bg-borderColor pt-5 px-6 pb-6 rounded-b-md">
-        <div className=" box-border hover:text-mainColor">
-          <h3 className=" text-tWhite hover:text-mainColor text-2xl font-semibold ">
+      <div className=" bg-borderColor text-tWhite pt-5 px-6 pb-6 rounded-b-md">
+        <div className=" box-border">
+          <h3 className=" text-tWhite hover:text-mainColor text-2xl font-semibold transition-all duration-300 ease-in-out delay-0">
             {title}
           </h3>
         </div>
