e220e1ed82474d8853607b6d740b81cde4dfa5f1
DogukanTansuk <dogukantansuk@gmail.com>
2023-09-24 17:29:59 +0300

    :art: Head değişim örneği

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Head from "next/head";
 import "../../styles/global.css";
 import { HomePage } from "./components/home";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Esra Tansuk | Personal Portfolio",
+};
 
 export default function Home() {
   return <HomePage />;
